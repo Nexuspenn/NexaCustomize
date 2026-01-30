3bcff9ef350c71823d274b462b5e4b36e90adb94
Kenneth Daily <kennydaily183@gmail.com>
2026-01-30 16:28:10 -0500

    Update application ID to org.nexuspenn.nexaos.customize

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 APPID="org.nexuspenn.nexaos.customize"
 BUNDLENAME="nexacustomize.flatpak"
-flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
+flatpak-builder --install --user --force-clean flatpak-app "$APPID".yml
 
 if [ "$1" = "bundle" ]; then
 	flatpak build-bundle ~/.local/share/flatpak/repo "$BUNDLENAME" "$APPID"
